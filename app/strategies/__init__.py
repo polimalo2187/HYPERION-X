5ad1ae164b33097c70fd2e3f90956f5017ca619f
polimalo2187 <carloramirez834@gmail.com>
2026-04-13 18:13:32 -0400

    Add files via upload

--- app/strategies/__init__.py
+++ app/strategies/__init__.py
@@ -2,9 +2,12 @@
 
 from app.strategies.base import BaseStrategy
 from app.strategies.registry import StrategyRegistry, get_strategy_registry
+from app.strategies.router import StrategyRouter, get_strategy_router
 
 __all__ = [
     "BaseStrategy",
     "StrategyRegistry",
     "get_strategy_registry",
+    "StrategyRouter",
+    "get_strategy_router",
 ]
