a1c7adb43aa58dbc22ba42448d96eefb42427ed3
polimalo2187 <carloramirez834@gmail.com>
2026-04-13 17:59:38 -0400

    Add files via upload

--- app/regime/__init__.py
+++ app/regime/__init__.py
@@ -1,1 +1,25 @@
+from app.regime.detector import (
+    DETECTOR_VERSION,
+    REGIME_RANGE,
+    REGIME_TREND,
+    REGIME_UNKNOWN,
+    REGIME_VOLATILE,
+    classify_candidate_regime,
+    detect_regime,
+)
+from app.regime.features import BTC_REFERENCE_SYMBOL, build_regime_features
+from app.regime.state_machine import RegimeStateMachine, advance_regime_state
 
+__all__ = [
+    "BTC_REFERENCE_SYMBOL",
+    "DETECTOR_VERSION",
+    "REGIME_RANGE",
+    "REGIME_TREND",
+    "REGIME_UNKNOWN",
+    "REGIME_VOLATILE",
+    "RegimeStateMachine",
+    "advance_regime_state",
+    "build_regime_features",
+    "classify_candidate_regime",
+    "detect_regime",
+]
